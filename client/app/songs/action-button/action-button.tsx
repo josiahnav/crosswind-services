801078c1ee0301ac1835a283b91f75e4b0777c6d
josiahnav <josiahnav24@gmail.com>
2023-11-19 06:32:17 -0700

    Added modal components

--- client/app/songs/action-button/action-button.tsx
+++ client/app/songs/action-button/action-button.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import React from 'react';
 
-export default function ActionButton(props: { title: string, subtitle: string }) {
+export default function ActionButton(props: { title: string, subtitle: string, onClick: () => void }) {
     return (
         <div
             className="transition border border-blue-500 rounded w-56 py-6 pl-5 cursor-pointer
-                hover:bg-blue-300 hover:border-blue-300">
+                hover:bg-blue-300 hover:border-blue-300" id="test" onClick={props.onClick}>
             <div className="font-bold text-3xl">{props.title}</div>
             <div>{props.subtitle}</div>
         </div>
--- client/app/songs/page.tsx
+++ client/app/songs/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Metadata} from "next";
 import SongForTableDto from "@/models/songs/SongForTableDto";
 import SongTable from "@/app/songs/song-table/song-table";
-import ActionButton from "@/app/songs/action-button/action-button";
+import SongActions from "@/app/songs/song-actions/song-actions";
 
 export const metadata: Metadata = {
     title: 'Songs'
@@ -15,13 +15,11 @@ async function getData(): Promise<SongForTableDto[]> {
 
 export default async function SongsPage() {
     const data = await getData();
+
     return (
         <div>
             <h2 className="text-2xl mb-6">What would you like to do...?</h2>
-            <div className="flex flex-row gap-6 mb-10">
-                <ActionButton title="Add" subtitle="a new worship song"></ActionButton>
-                <ActionButton title="Schedule" subtitle="a Sunday worship song"></ActionButton>
-            </div>
+            <SongActions></SongActions>
             <SongTable songs={data}></SongTable>
         </div>
     );

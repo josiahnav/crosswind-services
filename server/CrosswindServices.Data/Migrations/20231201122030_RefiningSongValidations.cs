8b2d61d56bccb54459c5c4f70deaed12588ebd4d
josiahnav <josiahnav24@gmail.com>
2023-12-01 05:22:40 -0700

    Created new Songs migration that refines its validations

---
+++ server/CrosswindServices.Data/Migrations/20231201122030_RefiningSongValidations.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrosswindServices.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefiningSongValidations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_scheduled",
+                table: "songs",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "composer",
+                table: "songs",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "bpm",
+                table: "songs",
+                type: "DECIMAL(5,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(5,2)",
+                oldDefaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_scheduled",
+                table: "songs",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "composer",
+                table: "songs",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "bpm",
+                table: "songs",
+                type: "DECIMAL(5,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "DECIMAL(5,2)",
+                oldNullable: true);
+        }
+    }
+}

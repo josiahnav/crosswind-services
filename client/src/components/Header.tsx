f7702fa32e47cf6d130fde0d778f0e7018518032
josiahnav <josiahnav24@gmail.com>
2023-12-11 05:28:46 -0700

    Added logo and links to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,11 @@
 export default function Header() {
     return (
-        <header className="h-11 bg-blue-500 mb-10"></header>
+        <header className="h-11 bg-blue-500 mb-10 px-4 flex flex-row items-center">
+            <img src="/logo.svg" alt="Crosswind Logo" className="h-5/6 cursor-pointer"/>
+            <div className="grow flex flex-row justify-end gap-5 text-white">
+                <span className="cursor-pointer">Songs</span>
+                <span className="cursor-pointer">Log Out</span>
+            </div>
+        </header>
     );
 }

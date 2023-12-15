57c5db72f3917959a2f9330caef0791dcfb9d6d8
josiahnav <josiahnav24@gmail.com>
2023-12-15 05:07:43 -0700

    Added react-router-dom and added route for songs page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
 import SongsPage from "./pages/SongsPage/SongsPage.tsx";
 import Header from "./components/Header.tsx";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {path: 'songs', element: <SongsPage/>}
+]);
 
 function App() {
     return (
         <>
             <Header></Header>
-            <SongsPage></SongsPage>
+            <RouterProvider router={router}></RouterProvider>
         </>
     )
 }
